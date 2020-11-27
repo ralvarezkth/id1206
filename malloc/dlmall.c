f3620b5d6fb2e9a81fc8e9edbcc33f407f2236f9
kthfre <golman@kth.se>
2020-11-27 01:05:43 +0100

    add cache, some refactoring

--- malloc/dlmall.c
+++ malloc/dlmall.c
@@ -3,6 +3,7 @@
 #include <sys/mman.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -13,6 +14,8 @@
 #define ALIGN 8
 #define ARENA (64*1024)
 #define MAXLIST 128
+#define CACHE_ENABLED 1
+#define CACHE_LIMIT 3
 
 struct head {
     uint16_t bfree; // 2 bytes, the status of block before
@@ -21,6 +24,10 @@ struct head {
     uint16_t size;  // 2 bytes, the size (max 2^16 i.e. 64 Ki byte)
     struct head *next;  // 8 bytes pointer
     struct head *prev;  // 8 bytes pointer
+};
+
+struct cache {
+    struct head *l_cache[CACHE_LIMIT];
 };
 
 struct head *arena = NULL;
@@ -33,10 +40,15 @@ int flist_8_count = 0;
 int flist_16_count = 0;
 int flist_32_count = 0;
 int flist_64_count = 0;
+struct cache list_cache[4] = {{NULL, NULL, NULL}, {NULL, NULL, NULL}, {NULL, NULL, NULL}, {NULL, NULL, NULL}};
 struct head *after(struct head *block);
 
 struct head *getlist(int size, struct head *block);
+int getlistindex(struct head *block);
 int mergecheck(int size);
+void initlist(struct head *list);
+struct head *shift(struct head *cache[]);
+void push(struct head *cache[], struct head *block);
 
 void detach(struct head *block) {
 
@@ -52,7 +64,26 @@ void detach(struct head *block) {
         }
         else {
             next->prev = NULL;
-            flist = next;
+            if (block == flist) {
+                flist = next; 
+                
+            } 
+            if (block == flist_8) {
+               flist_8 = next; 
+               
+            } 
+            if (block == flist_16) {
+                flist_16 = next; 
+                
+            } 
+            if (block == flist_32) {
+                flist_32 = next; 
+                
+            } 
+            if (block == flist_64) {
+                flist_64 = next; 
+                
+            }
 
             block->next = NULL;
             block->prev = NULL;
@@ -78,7 +109,21 @@ void insert(struct head *block) {
         block->next = target;
         block->prev = NULL;
         target->prev = block;
-        target = block;
+        if (target == flist) {
+            flist = block; 
+        } 
+        if (target == flist_8) {
+           flist_8 = block; 
+        } 
+        if (target == flist_16) {
+            flist_16 = block; 
+        } 
+        if (target == flist_32) {
+            flist_32 = block; 
+        } 
+        if (target == flist_64) {
+            flist_64 = block; 
+        } 
     }
     else if(target != NULL && target->next) {
         ;
@@ -167,69 +212,34 @@ struct head *find(uint16_t size) {
         flist_16 = find((16 + HEAD) * MAXLIST + 2 * HEAD);
         flist_32 = find((32 + HEAD) * MAXLIST + 2 * HEAD);
         flist_64 = find((64 + HEAD) * MAXLIST + 2 * HEAD);
-        flist->size = flist->size - HEAD;
-        flist_8->size = flist_8->size - HEAD;
-        flist_16->size = flist_16->size - HEAD;
-        flist_32->size = flist_32->size - HEAD;
-        flist_64->size = flist_64->size - HEAD;
-        struct head *aft = after(flist);
-        aft->bsize = flist->size;
-        aft->bfree = TRUE;
-        aft->size = 0;
-        aft->free = FALSE;
-        aft = after(flist_8);
-        aft->bsize = flist_8->size;
-        aft->bfree = TRUE;
-        aft->size = 0;
-        aft->free = FALSE;
-        aft = after(flist_16);
-        aft->bsize = flist_16->size;
-        aft->bfree = TRUE;
-        aft->size = 0;
-        aft->free = FALSE;
-        aft = after(flist_32);
-        aft->bsize = flist_32->size;
-        aft->bfree = TRUE;
-        aft->size = 0;
-        aft->free = FALSE;
-        aft = after(flist_64);
-        aft->bsize = flist_64->size;
-        aft->bfree = TRUE;
-        aft->size = 0;
-        aft->free = FALSE;
-        flist_8->free = TRUE;
-        flist_16->free = TRUE;
-        flist_32->free = TRUE;
-        flist_64->free = TRUE;
-        flist_8->bfree = FALSE;
-        flist_16->bfree = FALSE;
-        flist_32->bfree = FALSE;
-        flist_64->bfree = FALSE;
-        flist_8->bsize = 0;
-        flist_16->bsize = 0;
-        flist_32->bsize = 0;
-        flist_64->bsize = 0;
-        flist_8->next = NULL;
-        flist_8->prev = NULL;
-        flist_16->next = NULL;
-        flist_16->prev = NULL;
-        flist_32->next = NULL;
-        flist_32->prev = NULL;
-        flist_64->next = NULL;
-        flist_64->prev = NULL;
-        printf("list8 at %p free %d size %d\n", flist_8, flist_8->free, flist_8->size);
-        printf("list16 at %p free %d size %d\n", flist_16, flist_16->free, flist_16->size);
-        printf("list32 at %p free %d size %d\n", flist_32, flist_32->free, flist_32->size);
-        printf("list64 at %p free %d size %d\n", flist_64, flist_64->free, flist_64->size);
-        printf("listall at %p free %d size %d\n", flist, flist->free, flist->size);
+        initlist(flist);
+        initlist(flist_8);
+        initlist(flist_16);
+        initlist(flist_32);
+        initlist(flist_64);
+        printf("list_8 at %p free %d size %d\n", flist_8, flist_8->free, flist_8->size);
+        printf("list_16 at %p free %d size %d\n", flist_16, flist_16->free, flist_16->size);
+        printf("list_32 at %p free %d size %d\n", flist_32, flist_32->free, flist_32->size);
+        printf("list_64 at %p free %d size %d\n", flist_64, flist_64->free, flist_64->size);
+        printf("list_all at %p free %d size %d\n", flist, flist->free, flist->size);
     }
     struct head *next = getlist(size, NULL);
+    int index = getlistindex(getlist(size, NULL));
     struct head *block;
+
     while(next != NULL) {
+        if (list_cache[index].l_cache[0] != NULL && size == pow(2, index + 3)) {
+            struct head *block = shift(list_cache[index].l_cache);
+            detach(block);
+            block->free = FALSE;
+            struct head *aft = after(block);
+            aft->bfree = FALSE;
+
+            return block;
+        }
         if(next->size >= size) {
             if(next->size >= LIMIT(size)) {
                 block = split(next, size);
-                return block;
             }
             else {
                 block = next;
@@ -237,8 +247,9 @@ struct head *find(uint16_t size) {
                 block->free = FALSE;
                 struct head *aft = after(block);
                 aft->bfree = FALSE;
-                return block;
             }
+
+            return block;
         }
         next = next->next;
     }
@@ -250,9 +261,9 @@ struct head *merge(struct head *block) {
     struct head *aft = after(block);
     struct head *bef = before(block);
 
-    if (!mergecheck) {
+ /*   if (!mergecheck) {
         return block;
-    }
+    } */
 
     if(block->bfree && aft->free) { // merge with blocks before and after
         bef->size += block->size + aft->size + 2*HEAD;
@@ -338,6 +349,12 @@ void dfree(void *memory) {
         
         if (getlist(block->size, NULL) == flist) {
             block = merge(block);
+        } else if (CACHE_ENABLED) {
+            int index = getlistindex(getlist(block->size, NULL));
+
+            if (list_cache[index].l_cache[2] == NULL && block->size == pow(2, index + 3)) {
+                push(list_cache[index].l_cache, block);
+            }
         }
 
         struct head *aft = after(block);
@@ -512,6 +529,10 @@ struct head *getlist(int size, struct head *block) {
     return list;
 }
 
+int getlistindex(struct head *block) {
+    return block == flist_8 ? 0 : (block == flist_16 ? 1 : (block == flist_32 ? 2 : 3));
+}
+
 int mergecheck(int size) {
 
     if ((size <= 8 && flist_8_count <= 128) || (size > 8 && size <= 16 && flist_16_count <= 128) ||
@@ -522,10 +543,44 @@ int mergecheck(int size) {
     return TRUE;
 }
 
+void initlist(struct head *list) {
+    list->size = list->size - HEAD;
+    struct head *aft = after(list);
+    aft->bsize = list->size;
+    aft->bfree = TRUE;
+    aft->size = 0;
+    aft->free = FALSE;
+    list->free = TRUE;
+    list->bfree = FALSE;
+    list->bsize = 0;
+    list->next = NULL;
+    list->prev = NULL;
+}
+
+struct head *shift(struct head *cache[]) {
+    struct head *temp = cache[0];
+
+    for (int i = 1; i < CACHE_LIMIT; i++) {
+        cache[i - 1] = cache[i];
+    }
+    cache[CACHE_LIMIT - 1] = NULL;
+
+    return temp;
+}
+
+void push(struct head *cache[], struct head *block) {
+    for (int i = 0; i < CACHE_LIMIT; i++) {
+        if (cache[i] == NULL) {
+            cache[i] = block;
+            break;
+        }
+    }
+}
+
 void looksie() {
     struct head *test = flist;
 
-    for (int i = 0; i < 32; i++) {   
+    for (int i = 0; i < 18; i++) {   
         printf("nexxe%d %p size %d free %d bsize %d bfree %d\n", i, test, test->size, test->free, test->bsize, test->bfree);
         test = after(test);
     }
